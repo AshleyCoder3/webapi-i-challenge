c3a24ed60472ddc9ae7fb9a9b9b83474bf1db7f1
Ashley Burns <Ash_burn999@yahoo.com>
2021-08-29 20:25:12 -0400

    '//'

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samewise Gamgee'
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins'
+        }
+    ]
+    // json api instead of just .send(hobbits)
+    res.status(200).json(hobbits)
+})
+
+
+
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
 server.listen(8000, () => console.log('API running on port 8000'));
